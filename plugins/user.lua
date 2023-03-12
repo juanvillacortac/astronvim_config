75eebdaccd7f3a3c33d2cde1927249bb64336340
Juan Villacorta <juanvillacortac@gmail.com>
2023-03-12 02:19:48 -0400

    Migrated

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,33 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" }
+  },
+
+  -- We also support a key value style plugin definition similar to NvChad:
+  -- ["ray-x/lsp_signature.nvim"] = {
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  { "rebelot/kanagawa.nvim" },
+  { "shortcuts/no-neck-pain.nvim" },
+  { "petertriho/nvim-scrollbar",
+    requires = { "lewis6991/gitsigns.nvim" }
+  },
+  { "kevinhwang91/nvim-hlslens" },
+  { "mattn/flappyvird-vim" },
+  { "nvim-rogue/rogue.nvim" },
+  { "alec-gibson/nvim-tetris" },
+  -- { "lewis6991/gitsigns.nvim" },
 }
