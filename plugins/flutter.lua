ff5ac8800511a6e80f75e87a92984a07505c1f27
Juan Villacorta <juanvillacortac@gmail.com>
2023-03-12 20:24:28 -0400

    ...

--- plugins/flutter.lua
+++ plugins/flutter.lua
@@ -1,5 +1,12 @@
 return {
   {
     "akinsho/flutter-tools.nvim",
+    opts = function(opts)
+      opts.debugger = {
+        enabled = true,
+        run_via_dap = true,
+      }
+      return opts
+    end
   }
 }
